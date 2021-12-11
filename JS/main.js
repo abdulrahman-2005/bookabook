621c6dae952f659130ea91bdd00268cc6dea7ae8
Abdulrahman-2005 <bodyazmy.new.2005@gmail.com>
2021-12-11 19:01:52 +0200

    added functional searchability added alerting on availability added custom choosing for search settings

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,37 @@
 const container = document.getElementById("container");
 let searchBox = document.getElementById("search-box");
-const searchButton = document.getElementById("search");
+const searchButton = document.getElementById("search-settings");
 let template = ``;
+const pop = document.getElementById("sPop");
+let pointer = "";
+
+let poped = 0;
+const choices = {
+	title: "عنوان",
+	author: "مؤلف",
+	publisher: "ناشر",
+	isbn: "ISBN",
+	pages: "عدد صفحات",
+	by: "مشارك",
+};
+
+let option = "title";
+function searchSet(choice = option) {
+	switch (poped) {
+		case 1:
+			pop.style.zIndex = "-10";
+			poped = 0;
+			break;
+		case 0:
+			pop.style.zIndex = "10";
+			poped = 1;
+			break;
+		default:
+			break;
+	}
+	searchBox.placeholder = `ابحث عن ${choices[choice]}`;
+	option = choice;
+}
 
 function CreateBookTemplate() {
 	let keys = [];
@@ -14,18 +44,21 @@ function CreateBookTemplate() {
 		let by = people[books[book]["by"]];
 		template = `
         <div class="card">
-            <img src="assets/front/${books[book]["title"]}.jpg" alt="">
-            <img src="assets/backward/${books[book]["title"]}.jpg" style="left: 59%" alt="">
-            <div class="info-container">
-				<p >العنوان: ${books[book]["title"]}</p>
-				<p >المؤلف: ${books[book]["author"]}</p>
-				<p >دار النشر: ${books[book]["publisher"]}</p>
-				<p >الترقيم الدولي: ${books[book]["ISBN"]}</p>
-				<p >عدد الصفحات: ${books[book]["pages"]}</p>
-			    غير متاح   <button >بادل</button>
-            </div>
-			<a href="contributers/${by}.html" target="_blank"><span>${books[book]["by"]}</span></a>
-        </div>`;
+				<img src="assets/front/${books[book]["title"]}.jpg" alt="">
+				<img src="assets/backward/${books[book]["title"]}.jpg" style="left: 59%" alt="">
+				<div class="info-container">
+					<p >العنوان: ${books[book]["title"]}</p>
+					<p >المؤلف: ${books[book]["author"]}</p>
+					<p >دار النشر: ${books[book]["publisher"]}</p>
+					<p >الترقيم الدولي: ${books[book]["ISBN"]}</p>
+					<p >عدد الصفحات: ${books[book]["pages"]}</p>
+					<button style="color: ${books[book]["availability"][1]}; ${books[book]["availability"][1] == "green"? "cursor: pointer;" : ""}" 
+					${books[book]["availability"][0] == "غير متاح" ? "onclick='ANA()'": ""}>بادل</button>
+				</div>
+				<a href="contributers/${by}.html" target="_blank"><span>${
+				books[book]["by"]
+			}</span></a>
+			</div>`;
 		container.innerHTML += template;
 	}
 }
@@ -45,28 +78,46 @@ function fixSearch(text) {
 	return text;
 }
 
+function condition(opt, th, q) {
+	if (opt === "pages") {
+		return th === q;
+	} else {
+		return th.includes(q);
+	}
+}
+
 function search() {
 	query = searchBox.value;
-	query = fixSearch(query)
+	query = fixSearch(query);
 	container.innerHTML = "";
+	let thing;
 	Object.entries(books).forEach(([k]) => {
-		if (k.includes(query)) {
-			searchBox.style.border = "3px solid lightgreen";
+		thing = books[k][option];
+
+		if (condition(option, thing, query)) {
+			searchBox.style.border = "3px solid green";
 			let by = people[books[k]["by"]];
 			container.innerHTML += `
-                <div class="card">
-                    <img src="assets/front/${books[k]["title"]}.jpg" alt="">
-                    <img src="assets/backward/${books[k]["title"]}.jpg" style="left: 59%" alt="">
-                    <div class="info-container">
-                        <p >العنوان: ${books[k]["title"]}</p>
-                        <p >المؤلف: ${books[k]["author"]}</p>
-                        <p >دار النشر: ${books[k]["publisher"]}</p>
-                        <p >الترقيم الدولي: ${books[k]["ISBN"]}</p>
-                        <p >عدد الصفحات: ${books[k]["pages"]}</p>
-                        <a href="contributers/${by}.html" target="_blank"><span>${books[book]["by"]}</span></a>
-                        غير متاح   <button >تشارك</button>
-                    </div>
-                </div>`;
+			<div class="card">
+				<img src="assets/front/${books[book]["title"]}.jpg" alt="">
+				<img src="assets/backward/${books[book]["title"]}.jpg" style="left: 59%" alt="">
+				<div class="info-container">
+					<p >العنوان: ${books[book]["title"]}</p>
+					<p >المؤلف: ${books[book]["author"]}</p>
+					<p >دار النشر: ${books[book]["publisher"]}</p>
+					<p >الترقيم الدولي: ${books[book]["ISBN"]}</p>
+					<p >عدد الصفحات: ${books[book]["pages"]}</p>
+					<button style="color: ${books[book]["availability"][1]};
+					${books[book]["availability"][1] == "green"
+					? "cursor: pointer;"
+					: ""}" ${books[book]["availability"][0] === "غير متاح"
+					? ""
+					: "onclick='ANA()'"}>بادل</button>
+				</div>
+				<a href="contributers/${by}.html" target="_blank"><span>${
+				books[book]["by"]
+			}</span></a>
+			</div>`;
 		}
 	});
 	if (container.innerHTML === "") {
@@ -75,10 +126,11 @@ function search() {
 	}
 }
 
-searchButton.addEventListener("click", search);
 searchBox.addEventListener("keyup", () => {
 	searchBox.style.border = "3px solid yellow";
-});
-searchBox.addEventListener("keyup", () => {
 	search();
 });
+
+function ANA() {
+	alert("هذا الكتاب غير متاح حاليا")
+}
